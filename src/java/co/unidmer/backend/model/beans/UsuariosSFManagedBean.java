7112ddd9ccee1ef24d470adf71f50a4b1661071c
USUARIO <USUARIO@10.1.237.88>
2016-12-06 08:14:34 -0500

    agrego managed bean Usuarios

---
+++ src/java/co/unidmer/backend/model/beans/UsuariosSFManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unidmer.backend.model.beans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Named(value = "usuariosSFManagedBean")
+@Dependent
+public class UsuariosSFManagedBean {
+
+    /**
+     * Creates a new instance of UsuariosSFManagedBean
+     */
+    public UsuariosSFManagedBean() {
+    }
+    
+}
